575c2912e30e5bc2c5d3176dc98964b036b52276
Bridger Tower <bridgertower@gmail.com>
2022-12-06 16:35:20 -0700

    Updated

--- components/Cover.tsx
+++ components/Cover.tsx
@@ -3,11 +3,11 @@ import Image from 'next/image';
 export default function Cover() {
 	return (
 		<div className="bg-image w-screen overflow-hidden h-screen block z-50 lg:-mx-24 -m-6 bg-['https://btower.dev/btowerdev.jpg']">
-			<div className="absolute lg:invert left-1/2 -ml-40 top-1/2 -mt-20 text-center w-80 h-40">
+			<div className="absolute lg:invert left-1/2 -ml-40 top-1/3 lg:top-1/2 -mt-20 text-center w-80 h-40">
 				<p className="text-5xl mb-6">Bridger Tower</p>
 				<p>Designer and Developer</p>
 			</div>
-			<div className='absolute bottom-6 left-1/2 w-4 -ml-2 animate-pulse'>
+			<div className='absolute lg:invert bottom-6 left-1/2 w-4 -ml-2 animate-pulse'>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					fill="none"
